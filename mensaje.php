a66bb84cc71d73cf312dc1bf21853871a2e68f67
virtualcondor <virtualcondor@gmail.com>
2025-06-26 16:20:47 -0400

    Add files via upload

---
+++ mensaje.php
@@ -0,0 +1,40 @@
+<?
+
+//obtener datos
+
+ $nombre= addslashes($_POST['nombre']);
+ $email= addslashes($_POST['mail']);
+ $msj= addslashes($_POST['mensaje']);
+
+
+require_once("function/class.phpmailer.php");
+require_once("function/class.smtp.php");
+
+// envio del email
+
+if($_REQUEST['enviar'])
+{
+ $contenido = "
+Nombre: "." ".$nombre."\n
+E-mail: "." ".$email."\n
+Mensaje: "." ".$msj;
+
+$para="contacto@virtualcondor.cl";
+$asunto  = 'Contacto Web Virtual Condor';
+$cabeceras  = 'MIME-Version: 1.0' . "\r\n";
+$cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$cabeceras .= 'From: Contacto Web Virtual Condor <contacto@virtualcondor.cl>' . "\r\n" .
+   'Reply-To: contacto@virtualcondor.cl' . "\r\n" .
+   'X-Mailer: PHP/' . phpversion();
+   mail($para, $asunto, $contenido, $cabeceras);
+   if(mail($email, "Confirmación Contacto Web Virtual Condor", "Se ha enviado satisfactoriamente su mensaje. Nos pondremos en contacto con usted pronto. Atte Virtual Condor.",$cabeceras))
+   {
+	 ?>
+		<script language="javascript" type="text/javascript">
+        alert('Se ha enviado satisfactoriamente su mensaje. Nos pondremos en contacto con usted pronto. Atentamente Virtual Cóndor.');
+        window.location = 'index.html';
+        </script>
+	 <?
+	 }
+}
+?>
